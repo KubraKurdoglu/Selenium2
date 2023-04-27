15b5ac3226f8a400713dfbdb7607b26963178e43
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-04-27 22:38:35 +0200

    27 avril

--- src/test/java/techproed/tests/day23_DependsOnMethods/C04_DriverTest.java
+++ src/test/java/techproed/tests/day23_DependsOnMethods/C04_DriverTest.java
@@ -1,5 +1,6 @@
 package techproed.tests.day23_DependsOnMethods;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
@@ -9,6 +10,7 @@ public class C04_DriverTest {
         //Driver.getDriver().get("https://techproeducation.com");
         Driver.getDriver().get("https://amazon.com");
         Driver.getDriver().get("https://facebook.com");
+        Driver.getDriver().findElement(By.xpath("(//button[@value='1'])[3]")).click();
         Driver.getDriver().get(ConfigReader.getProperty("techproed_Url"));
         System.out.println(ConfigReader.getProperty("username"));
         System.out.println(ConfigReader.getProperty("password"));
