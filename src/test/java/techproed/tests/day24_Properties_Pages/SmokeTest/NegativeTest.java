b1a7723f673350f1fe6588e9a3722be88ad65949
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-05-08 15:48:04 +0200

    8 may

--- src/test/java/techproed/tests/day24_Properties_Pages/SmokeTest/NegativeTest.java
+++ src/test/java/techproed/tests/day24_Properties_Pages/SmokeTest/NegativeTest.java
@@ -1,6 +1,7 @@
 package techproed.tests.day24_Properties_Pages.SmokeTest;
 
 import org.openqa.selenium.Keys;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import techproed.pages.BlueRentalPage;
 import techproed.utilities.ConfigReader;
@@ -26,10 +27,13 @@ public class NegativeTest {
     public void test1() throws InterruptedException {
 
         Driver.getDriver().get(ConfigReader.getProperty("blueRentACarUrl"));
+        Reporter.log("BlueRental sayfasina gidildi");
         BlueRentalPage blueRentalPage =new BlueRentalPage();
         blueRentalPage.login.click();
+        Reporter.log("Login butonuna tiklandi");
         blueRentalPage.email.sendKeys(ConfigReader.getProperty("fakeEmail"), Keys.TAB,
                 ConfigReader.getProperty("fakepass"));
+        Reporter.log("Email ve password girildi");
 
         //burda Keys.ENTER ==> calismadi
         //o nedenle Login'in locate'ini aldi ve oyle click yapti
@@ -40,18 +44,24 @@ public class NegativeTest {
         //isDisplayed ile veya assertEquals ile yapilabilir
 
         blueRentalPage.login2.click();
+        Reporter.log("Tekrar Login butonuna tiklandi");
 
         //Hoca testbase'i kopyaladiktan sonra, adini ReusableMethods koydu, ve o class'taki methodlari kullandi
         ReusableMethods.bekle(3);
         ReusableMethods.tumSayfaResmi("TumSayfaResmi");
+        Reporter.log("Sayfanin resmi alindi");
 
         //Hatta burda, "tumSayfaResmi" methodunu kullaninca, otomatik TestOutput dosyasi olusturdu.
         //cunku o methodda dosya yolu vardi, methodu calistrinca dosyayi otomatik olusturdu
 
         assertTrue(blueRentalPage.hataMesaji.isDisplayed());
+        Reporter.log("Dogrulama yapildi");
         Driver.closeDriver();
+        Reporter.log("Sayfa kapatildi");
 
         //objeden cagirmamiz lazim; class'tan cagirirsak "satatic "ister
+
+        //Reporter ==> TestNG'den geliyor
 
 
 
